f658e30befdbf530a3a688d6731d10aa0d4f8772
Joseph DiCarlo <60683575+NearingIX@users.noreply.github.com>
2022-07-25 22:46:44 -0400

    Update String_to_Binary_Converter.cpp

--- String_to_Binary_Converter.cpp
+++ String_to_Binary_Converter.cpp
@@ -18,17 +18,14 @@ int main() {
 
     //Convert messageDigest to binary
     for (size_t i = 0; i < messageDigest.size(); i++) {
-        
        bitset<8> binaryDigest = bitset<8>(messageDigest[i]);
        bitVector.push_back(binaryDigest);
-       
     }
-
+    
     //Prints bitVector via range based for loop
     for (auto i: bitVector) {
         cout << i << ' ';
     }
-
+    
     return 0;
-
 }
